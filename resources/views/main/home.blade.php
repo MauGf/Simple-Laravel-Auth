4a86f0179ebc77e405bf65c5dca2100660e1e356
MauGf <maugarciaflores@gmail.com>
2017-04-27 19:48:21 -0600

    Actualizado

---
+++ resources/views/main/home.blade.php
@@ -0,0 +1,15 @@
+@extends( 'layouts.app')
+
+
+
+
+@section('title',  'My Application')
+
+
+
+
+@section('content')
+<div class="container">
+	<h1>Bienvenidos estamos probando Laravel 5.4</h1>
+</div>
+@endsection
